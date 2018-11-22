035a29b14bd3066d146c49852404e68d673e9edb
moroclash <mmoroclash@gmail.com>
2018-11-22 22:02:14 +0200

    update and test some cases

--- js/code2.js
+++ js/code2.js
@@ -28,8 +28,8 @@ function generat_head_cell(text,class_s,data_column,id,tashkeel_flage){
     // console.log();
     if(tashkeel_flage)
     {
-	content = "<table><tr><th id=\"character-text\" class=\"column100\">"+text+"</th></tr></table>"
-	return "<th id=\""+id+"\" class=\""+class_s+" table_char  no_pading stickyheader hov_cell\" data-column=\""+data_column+"\">"+content+"</th>"
+	//content = "<table><tr><th id=\"character-text\" class=\"column100\">"+text+"</th></tr></table>"
+	return "<th id=\""+id+"\" class=\""+class_s+" table_char  no_pading stickyheader hov_cell character_text\" data-column=\""+data_column+"\">"+text+"</th>"
     }
     return "<th id=\""+id+"\" class=\""+class_s+" stickyheader hov_cell\" data-column=\""+data_column+"\">"+text+"</th>"
 }
@@ -63,7 +63,7 @@ function show_all_info_mode(){
         table_chars_content = "<table id=\""+table_char_id+"\" class=\"char_counts_table table_char all_aya clicked"+j+"\">"+char_content+"</table>"
         table_content = "<tr class=\"all_aya_0\"><td>"+table_chars_content+"</td></tr>"
 	alphabet_content = "<table class=\"all_table\">"+table_content+"</table>"
-	all_body += "<td class=\"column100 column"+(j+3)+" char_cell\" data-column=\"column"+(j+3)+"\">"+alphabet_content+"</td>"
+	all_body += "<td class=\"column100 column_"+(j+3)+" char_cell\" data-column=\"column"+(j+3)+"\">"+alphabet_content+"</td>"
     }
     //merge all html
     sura_html = "<tr class=\"row100 all\">" +all_body+ "</tr>"
@@ -81,32 +81,29 @@ function generat_sura_row(sura_info,start_pos,end_pos){
   table_content = ""
   for(j = start_pos; j < end_pos; j++){
     word_id = sura_id+"_word_"+j
-    table_content += "<tr id=\""+word_id+"\" class=\"word"+j+" hov_cell word\"><td>"+sura_info.unique_words[j]+"</td></tr>"
+    table_content += "<tr id=\""+word_id+"\" class=\"word"+j+" hov_cell word\"><td>"+ (j+1) + " : " + sura_info.unique_words[j]+"</td></tr>"
   }
-    content.push(table_content)
-  ayat_small_table = "<table class=\""+sura_info.sura_name+"_table word\">"+table_content+"</table>"
-  sura_body = "<td class=\"column100 column1 "+sura_info.sura_name+" char_cell\" data-column=\"column1\">"+ayat_small_table+"</td>"
+  content.push(table_content)
+  ayat_small_table = "<table class=\""+sura_info.sura_name.replace(' ','')+"_table word\">"+table_content+"</table>"
+  sura_body = "<td class=\"column100 column_1 "+sura_info.sura_name.replace(' ','')+" char_cell\" data-column=\"column1\">"+ayat_small_table+"</td>"
   //*****************************************************************
 
-  //*************make char's cell ************************************
+  //************* make char's cell ************************************
   for(j = 0; j < sura_info.ayat_num; j++){
     table_content = ""
     for(n = start_pos; n < end_pos; n++){
       aya = sura_info.ayat[j]
-      words_counts ="<td id=\"tashkeel_shape_count\" class=\"column100\">"+aya.frequency[n]+"</td>"
-      char_content = "<tr>"+words_counts+"</tr>"
       table_char_id = sura_id+"_word_"+n+"_aya_"+j
-      table_chars_content = "<table id=\""+table_char_id+"\" class=\"char_counts_table table_char "+sura_id+"_word_"+n+" clicked"+j+"\">"+char_content+"</table>"
-      table_content += "<tr class=\"word"+n+"\"><td>"+table_chars_content+"</td></tr>"
+      table_content += "<tr id=\""+table_char_id+"\" class=\"word"+n+" column100 char_counts_table table_char hov_cell "+sura_id+"_word_"+n+" clicked"+j+"\"><td class=\"column100\">"+aya.frequency[n]+"</td></tr>"
     }
     content.push(table_content)
-    alphabet_content = "<table class=\""+sura_info.sura_name+"_table\">"+table_content+"</table>"
-    sura_body += "<td class=\"column100 column"+(j+2)+" "+sura_info.sura_name+" char_cell\" data-column=\"column"+(j+2)+"\">"+alphabet_content+"</td>"
-    }
+    alphabet_content = "<table class=\""+sura_info.sura_name.replace(' ','')+"_table\">"+table_content+"</table>"
+    sura_body += "<td class=\"column100 column_"+(j+2)+" "+sura_info.sura_name.replace(' ','')+" char_cell\" data-column=\"column"+(j+2)+"\">"+alphabet_content+"</td>"
+  }
   //*****************************************************************
 
   //merge all html
-  sura_html = "<tr class=\"row100 "+sura_info.sura_name+"\">" +sura_body+ "</tr>"
+  sura_html = "<tr class=\"row100 "+sura_info.sura_name.replace(' ','')+"\">" +sura_body+ "</tr>"
   return [sura_html,content]
 }
 
@@ -330,7 +327,7 @@ var main = function(){
   var start = 2
   sura = Quran_.swar[current_sura-1]
   for (i = 0; i < sura.ayat_num ; i++) {
-    $('.head').append(generat_head_cell(i+1 ,"column100 column"+(i+start),'column'+(i+start),"aya_"+i,true));
+    $('.head').append(generat_head_cell((i+1) ,"column100 column"+(i+start),'column'+(i+start),"aya_"+i,true));
   }
 
   if(current_sura != 0){
@@ -364,8 +361,6 @@ var main = function(){
     $('.swar').append(show_all_info_mode())
     all_info_mode = true
   }
-
-
 
   $('.hov_cell , .table_char , .head.column100').click(function() {
     console.log($(this))
@@ -414,9 +409,9 @@ var main = function(){
 
 
 // to make dinamic rendering
-start_with_ayat_num = 15
+start_with_ayat_num = 14
 end_render = 0
-step_render = 5
+step_render = 4
 curent_sura_num = 0
 num_of_steps_down = 0
 num_of_steps_up = 0
@@ -437,7 +432,7 @@ $(document).ready(function() {
     // Vertical end reached?
     var st = $(this).scrollTop();
     //check if scroll down
-    if($('.table100').height() - win.height() < win.scrollTop() && !all_info_mode){
+    if(Math.abs($('.table100').height() - win.height()) < win.scrollTop() && !all_info_mode){
       check_if_append_nodes_down = false;
       check_if_append_nodes_up = false;
       // downscroll code
@@ -445,9 +440,9 @@ $(document).ready(function() {
       {
         // check if exist enough ayat to genrate
         if((end_render+step_render <= Quran_.swar[curent_sura_num].unique_words.length)){
-          ayat_content = generat_sura_row(Quran_.swar[curent_sura_num], end_render, end_render+step_render)[1]
-          for( i=0; i< ayat_content.length; i++){
-            $(".column"+(i+1)).find("."+Quran_.swar[curent_sura_num].sura_name+"_table").children('tbody').append(ayat_content[i])
+          ayat_content_ = generat_sura_row(Quran_.swar[curent_sura_num], end_render, end_render+step_render)[1]
+          for( i=0; i<ayat_content_.length; i++){
+            $(".column_"+(i+1)).find("."+Quran_.swar[curent_sura_num].sura_name.replace(' ','')+"_table").children('tbody').append(ayat_content_[i])
           }
           end_render += step_render
           num_of_steps_down +=1
@@ -456,12 +451,10 @@ $(document).ready(function() {
         }
         //check if no enough ayat to genrate
         else if((end_render+step_render > Quran_.swar[curent_sura_num].unique_words.length)){
-
           if( Quran_.swar[curent_sura_num].unique_words.length != end_render ){
-
-            ayat_content = generat_sura_row(Quran_.swar[curent_sura_num], end_render, Quran_.swar[curent_sura_num].unique_words.length)[1]
-            for( i=0; i< ayat_content.length; i++){
-               $(".column"+(i+1)).find("."+Quran_.swar[curent_sura_num].sura_name+"_table").children('tbody').append(ayat_content[i])
+            ayat_content_ = generat_sura_row(Quran_.swar[curent_sura_num], end_render, Quran_.swar[curent_sura_num].unique_words.length)[1]
+            for( i=0; i< ayat_content_.length; i++){
+               $(".column_"+(i+1)).find("."+Quran_.swar[curent_sura_num].sura_name.replace(' ','')+"_table").children('tbody').append(ayat_content_[i])
             }
             num_of_rest_ayat_that_not_visualised = step_render - (Quran_.swar[curent_sura_num].unique_words.length - end_render);
             ayat_in_table += (step_render-num_of_rest_ayat_that_not_visualised)
@@ -481,22 +474,22 @@ $(document).ready(function() {
           }
           num_of_steps_down +=1
           check_if_append_nodes_down = true;
-         }
+        }
         // check if append nodes down to remove some nodes from top
-        if(check_if_append_nodes_down && ayat_in_table >= 25+step_render){
-          children_nodes = $(".column"+(1)).find("."+Quran_.swar[sura_num_to_remove_from_top].sura_name+"_table").children('tbody').children('tr')
+        if(check_if_append_nodes_down && ayat_in_table >= 16+step_render){
+          children_nodes = $(".column_"+(1)).find("."+Quran_.swar[sura_num_to_remove_from_top].sura_name.replace(' ','')+"_table").children('tbody').children('tr')
           if (children_nodes.length > step_render){
-            for (i=0; i< (Quran_.systems_info[current_system]+2); i++){
-              children_nodes = $(".column"+(i+1)).find("."+Quran_.swar[sura_num_to_remove_from_top].sura_name+"_table").children('tbody').children('tr')
+            for (i=0; i< (Quran_.swar[curent_sura_num].ayat_num+1); i++){
+              children_nodes = $(".column_"+(i+1)).find("."+Quran_.swar[sura_num_to_remove_from_top].sura_name.replace(' ','')+"_table").children('tbody').children('tr')
               children_nodes.slice(0,step_render).remove();
             }
           }
           else{
             num_of_rest_nodes = step_render - children_nodes.length
-            $(".swar").find("."+Quran_.swar[sura_num_to_remove_from_top].sura_name).remove();
+            $(".swar").find("."+Quran_.swar[sura_num_to_remove_from_top].sura_name.replace(' ','')).remove();
             sura_num_to_remove_from_top +=1
-            for (i=0; i< (Quran_.systems_info[current_system]+2); i++){
-                 children_nodes = $(".column"+(i+1)).find("."+Quran_.swar[sura_num_to_remove_from_top].sura_name+"_table").children('tbody').children('tr')
+            for (i=0; i< (Quran_.swar[curent_sura_num].ayat_num+1); i++){
+                 children_nodes = $(".column_"+(i+1)).find("."+Quran_.swar[sura_num_to_remove_from_top].sura_name.replace(' ','')+"_table").children('tbody').children('tr')
               children_nodes.slice(0,num_of_rest_nodes).remove();
             }
           }
@@ -511,16 +504,17 @@ $(document).ready(function() {
         });
         scroll_down_done = true;
       }
-      // check if scroll up
+
+    // check if scroll up
     } else if((st < 90 && !all_info_mode) || (st - lastScrollTop < 0 && st < 90 && scroll_down_done && !all_info_mode)){
-      children_nodes = $(".column"+(1)).find("."+Quran_.swar[sura_num_to_remove_from_top].sura_name+"_table").children('tbody').children('tr')
+      children_nodes = $(".column_"+(1)).find("."+Quran_.swar[sura_num_to_remove_from_top].sura_name.replace(' ','')+"_table").children('tbody').children('tr')
       first_aya = (parseInt(children_nodes.slice(0,1).attr('id').split("_")[3])+1);
       if (!the_begin_of_ayat){
         //check if exsist enough ayat to render up
         if( first_aya > step_render){
           ayat_content = generat_sura_row(Quran_.swar[sura_num_to_remove_from_top], first_aya-step_render-1, first_aya-1)[1]
           for( i=0; i< ayat_content.length; i++){
-            $(".column"+(i+1)).find("."+Quran_.swar[sura_num_to_remove_from_top].sura_name+"_table").children('tbody').prepend(ayat_content[i])
+            $(".column_"+(i+1)).find("."+Quran_.swar[sura_num_to_remove_from_top].sura_name.replace(' ','')+"_table").children('tbody').prepend(ayat_content[i])
           }
           ayat_in_table += step_render
           num_of_steps_up += 1
@@ -531,7 +525,7 @@ $(document).ready(function() {
           {
             ayat_content = generat_sura_row(Quran_.swar[sura_num_to_remove_from_top], 0, first_aya-1)[1]
             for( i=0; i< ayat_content.length; i++){
-              $(".column"+(i+1)).find("."+Quran_.swar[sura_num_to_remove_from_top].sura_name+"_table").children('tbody').prepend(ayat_content[i])
+              $(".column_"+(i+1)).find("."+Quran_.swar[sura_num_to_remove_from_top].sura_name.replace(' ','')+"_table").children('tbody').prepend(ayat_content[i])
             }
             num_of_rest_ayat_that_not_visualised = step_render - first_aya
             ayat_in_table += (step_render-num_of_rest_ayat_that_not_visualised)
@@ -561,12 +555,12 @@ $(document).ready(function() {
         }//end of else of  no  enough ayat
 
         // check if append nodes down to remove some nodes from bottom
-        if(num_of_steps_up == 1 || ayat_in_table >= 25+step_render){
-          children_nodes = $(".column"+(1)).find("."+Quran_.swar[curent_sura_num].sura_name+"_table").children('tbody').children('tr')
+        if(num_of_steps_up == 1 || ayat_in_table >= 16+step_render){
+          children_nodes = $(".column_"+(1)).find("."+Quran_.swar[curent_sura_num].sura_name.replace(' ','')+"_table").children('tbody').children('tr')
           children_nodes_len = children_nodes.length
           if (children_nodes_len > step_render){
-            for (i=0; i< (Quran_.systems_info[current_system]+2); i++){
-              children_nodes = $(".column"+(i+1)).find("."+Quran_.swar[curent_sura_num].sura_name+"_table").children('tbody').children('tr')
+            for (i=0; i< (Quran_.swar[current_sura-1]+1); i++){
+              children_nodes = $(".column_"+(i+1)).find("."+Quran_.swar[curent_sura_num].sura_name.replace(' ','')+"_table").children('tbody').children('tr')
               children_nodes.slice(children_nodes_len-step_render,children_nodes_len).remove();
             }
             end_render -= step_render
@@ -577,7 +571,7 @@ $(document).ready(function() {
             num_of_rest_nodes = step_render - children_nodes_len
                if(num_of_rest_nodes != 0){
                  for (i=0; i< (Quran_.systems_info[current_system]+2); i++){
-                   children_nodes = $(".column"+(i+1)).find("."+Quran_.swar[curent_sura_num].sura_name+"_table").children('tbody').children('tr')
+                   children_nodes = $(".column_"+(i+1)).find("."+Quran_.swar[curent_sura_num].sura_name.replace(' ','')+"_table").children('tbody').children('tr')
                    children_nodes.slice(children_nodes.length - num_of_rest_nodes,children_nodes.length).remove();
                  }
                  end_render = Quran_.swar[curent_sura_num].ayat.length - num_of_rest_nodes
@@ -585,7 +579,6 @@ $(document).ready(function() {
             else {
               end_render = Quran_.swar[curent_sura_num].ayat.length
             }
-
           }
           num_of_steps_up = 0
           ayat_in_table -= step_render
